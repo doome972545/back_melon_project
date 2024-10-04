60eba6bfde79c47c818a200c199846e93c5c112d
chayanon <chayanon.au@rmuti.ac.th>
2024-10-04 18:38:40 +0700

    fix vercel success

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const connection = require("./config/db")
+const connection = require("../config/db")
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
-const fertilizer = require('./routes/house.routes')
-const auth = require('./routes/auth.routes')
-const admin = require('./routes/admin.routes')
+const fertilizer = require('../routes/house.routes')
+const auth = require('../routes/auth.routes')
+const admin = require('../routes/admin.routes')
 app.use(cors())
 app.use(express.json());
 app.use(express.static('./uploads'))
